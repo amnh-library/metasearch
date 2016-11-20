050ebc62370af4044281493bb1412fcf3ce648cc
amanda <pickering.amanda@gmail.com>
2016-11-19 22:11:11 -0500

    Form -> SearchForm rename

---
+++ library-metasearch/src/form/SearchForm.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react"
+import BiodiversityClient from '../client/BiodiversityClient'
+
+
+export default class SearchForm extends Component {
+  handleSubmit = event => {
+    alert("~~~~ submitted ~~~~ " + this.state.value)
+    this.props.handleSearchSubmit(this.state.)
+    event.preventDefault()
+
+  }
+
+  handleChange = event => {
+    this.setState({ value: event.target.value})
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = { value: "" }
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  render() {
+    const styles = {
+      inputContainer: {
+        margin: "4em",
+        display: "flex",
+        flexDirection: "column"
+      },
+      input: {
+        padding: ".3rem"
+      },
+      button: {
+        margin: "1em",
+        padding: "1em"
+      }
+    }
+    return (
+      <div style={styles.inputContainer} id="search-term-input">
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label style={styles.label}>
+              Search for something:
+            </label>
+          </div>
+          <input value={this.state.value} style={styles.input} type="text" name="search" onChange={this.handleChange} />
+          <input type="submit" value="Submit" style={styles.button} />
+        </form>
+      </div>
+    )
+  }
+}
