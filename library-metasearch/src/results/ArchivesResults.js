ea0dcda287328ea7afa363274cf44017e42b0409
fiono <frcondon@gmail.com>
2016-11-20 09:25:45 -0500

    Adding ArchiveSpace results integrated against local API instance

---
+++ library-metasearch/src/results/ArchivesResults.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default class ArchivesResults extends React.Component {
+  render() {
+    let results = this.props.results || {};
+
+    if (results.length > 0) {
+    return (
+      <div>
+        <div>
+        <h4>Matches:</h4>
+        {results.map(function (result, index) {
+          return (<div>
+            <a href={result.url}>{result.title}</a>
+          </div>)
+        })}
+        </div>
+      </div>
+    );
+    }
+  }
+}
