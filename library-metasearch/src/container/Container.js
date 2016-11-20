75199fb130cf3ec287eebf3f6e84450601146cf8
amanda <pickering.amanda@gmail.com>
2016-11-19 22:08:15 -0500

    container bugfixes

--- library-metasearch/src/container/Container.js
+++ library-metasearch/src/container/Container.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
-import SearchForm from '../form/Form'
+import React, { Component, PropTypes } from 'react'
+import SearchForm from '../form/SearchForm'
 import BiodiversityClient from '../client/BiodiversityClient'
+import BiodiversityResults from '../results/BiodiversityResults'
 import { run as biodiversityRun } from '../client/BiodiversityClient'
 
 
@@ -18,24 +19,28 @@ export default class Container extends Component {
   constructor() {
     super()
     // data received from api calls goes here, initially empty
-    this.setState({
+    this.state = {
       term: '',
       results: {}
-    })
+    }
   }
 
   handleSearchSubmit = term => {
     console.log('handleSearchSubmit too ' + term)
-    this.setState({ term })
-    // let _this = this
+    // this.setState({ term })
     // api functions called here, update state based on results (or error)
+    // apiRun(term).then(this.setState{ results })
   }
 
   render() {
-    const styles = {}
-    console.log(this.handleSearchSubmit)
+    const styles = {
+      container: {
+        display: "flex",
+        flexDirection: "column"
+      }
+    }
     return (
-      <div>
+      <div style={styles.container}>
         <SearchForm handleSubmit={this.handleSearchSubmit} />
         <BiodiversityResults term={this.state.value} results={this.state.results.biodiversity}/>
       </div>
