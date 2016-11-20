3396504c02e7d07a734d3747a36466caf7d096a4
amanda <pickering.amanda@gmail.com>
2016-11-20 01:15:03 -0500

    professional website is super real

--- library-metasearch/src/App.js
+++ library-metasearch/src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
 import Container from './container/Container'
 import './App.css'
+import hackTheStacks from './img/hack-the-stacks.png'
+import logo from './img/amnh-logo.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img className="App-header-img" src={require('./img/hack-the-stacks.png')} alt="Metasearch" />
+          <div>
+            <img className="App-header-logo" src={logo} alt="AMNH Logo" />
+          </div>
           <h2 className="App-title">Library Metasearch</h2>
+          <div>
+            <img className="App-header-img" src={hackTheStacks} alt="Metasearch" />
+          </div>
         </div>
         <div className="App-container">
           <Container />
