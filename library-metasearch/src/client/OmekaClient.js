f911f932803d5803d8ba2a73517b514dc09c8b25
mik3cap <mik3cap@gmail.com>
2017-06-27 16:09:32 -0400

    Hopefully fix for build errors

--- library-metasearch/src/client/OmekaClient.js
+++ library-metasearch/src/client/OmekaClient.js
@@ -1,14 +1,14 @@
 import $ from 'jquery';
 
-if (process.env.REACT_APP_ENVIRONMENT === 'development') {
-  const API_BASE_URL = 'http://api-dev.library.amnh.org/api/v1/images';
-} else {
-  const API_BASE_URL = 'http://api.library.amnh.org/api/v1/images';
-}
-
 const MAX_RESULTS = 4;
 
 function run(term) {
+  if (process.env.REACT_APP_ENVIRONMENT === 'development') {
+    const API_BASE_URL = 'http://api-dev.library.amnh.org/api/v1/images';
+  } else {
+    const API_BASE_URL = 'http://api.library.amnh.org/api/v1/images';
+  }
+
   return $.ajax({
     url: API_BASE_URL,
     data: {q:term},
--- library-metasearch/src/client/SierraClient.js
+++ library-metasearch/src/client/SierraClient.js
@@ -1,26 +1,24 @@
 import $ from 'jquery';
 
-if (process.env.REACT_APP_ENVIRONMENT === 'development') {
-  const API_BASE_URL = 'http://api-dev.library.amnh.org/api/v1/resources/sierra';
-} else {
-  const API_BASE_URL = 'http://api.library.amnh.org/api/v1/resources/sierra';
-}
-
-app.listen(process.env.REACT_APP_PORT);
-
 function run(term) {
-    return $.ajax({
-        method: 'get',
-        url: API_BASE_URL + "query",
-        data: {
-            offset: 0,
-            limit: 5,
-            term: term,
-        },
-        dataType: 'jsonp'
-    }).then((data, textStatus, jqXHR) => {
-        return data;
-    });
+  if (process.env.REACT_APP_ENVIRONMENT === 'development') {
+    const API_BASE_URL = 'http://api-dev.library.amnh.org/api/v1/resources/sierra';
+  } else {
+    const API_BASE_URL = 'http://api.library.amnh.org/api/v1/resources/sierra';
+  }
+
+  return $.ajax({
+    method: 'get',
+    url: API_BASE_URL + "query",
+    data: {
+      offset: 0,
+      limit: 5,
+      term: term,
+    },
+    dataType: 'jsonp'
+  }).then((data, textStatus, jqXHR) => {
+    return data;
+  });
 }
 
 export default {
